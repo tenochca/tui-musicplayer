8f616a0d744c8e9a7e7eb423337549e3aedc8078
Diego Izaguirre <tenochca.di@outlook.com>
2024-10-02 23:38:01 -0500

    Drop down menus list artist and albums in directory, also added comments for documentation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 mod tui;
 
 fn main() {
-    //tui::tui_run();
-    let music = r"C:\Users\tenoc\Music";
-    let folder = tui::list_folders(music);
-    println! ("{:?}", folder);
+    tui::tui_run();
 }
--- src/tui.rs
+++ src/tui.rs
@@ -2,17 +2,17 @@ use cursive::{event::Key, menu, views::Dialog};
 use std::fs;
 
 pub fn list_folders(music_directory: &str) -> Vec<String> {
-    fs::read_dir(music_directory)
-    .unwrap()
-    .filter_map(|entry| {
+    fs::read_dir(music_directory) //read the contents of the directory
+    .unwrap() //handles errors
+    .filter_map(|entry| { //iterates through directory
         let entry = entry.unwrap();
-        if entry.path().is_dir() {
-            Some(entry.file_name().into_string().unwrap())
+        if entry.path().is_dir() { //checks if entry is directory
+            Some(entry.file_name().into_string().unwrap()) //valid entries get turned into strings
         } else {
             None
         }
     })
-    .collect()
+    .collect() //collect all valid entries into Vec
 }
 
 
@@ -20,8 +20,32 @@ pub fn list_folders(music_directory: &str) -> Vec<String> {
 pub fn tui_run () {
     let mut siv = cursive::default();
 
-    siv.menubar().add_subtree("Albums", menu::Tree::new()) 
-    .add_subtree("Artists", menu::Tree::new())       
+    //hard coded path to music
+    let music_directory = r"C:\Users\tenoc\Music";
+
+    //Assume the folders in the dir are all artists & their sub folders are all albums
+    let artists = list_folders(music_directory);
+
+    let mut albums_menu = menu::Tree::new(); //instantiate empty menu to populate
+
+    for artist in &artists { //loop through the artist dirs and format new paths to pass to list_folders
+        let artist_directory = format!("{}/{}", music_directory, artist);
+        let albums = list_folders(&artist_directory);
+
+        for album in albums { //for each album we populate the menu with it
+            albums_menu.add_leaf(album, |_| {});
+        }
+    }
+        let mut artists_menu = menu::Tree::new(); //empty artist menu
+
+        for artist in artists { //populate artist menu
+            artists_menu.add_leaf(artist, |_| {});
+    }
+
+
+
+    siv.menubar().add_subtree("Albums", albums_menu) 
+    .add_subtree("Artists", artists_menu)       
     .add_delimiter()
     .add_leaf("Quit", |s| s.quit());
 
