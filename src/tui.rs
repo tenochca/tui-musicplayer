37fe6f7bed5c22dda64780c7936794e1a7034f98
Diego Izaguirre <tenochca.di@outlook.com>
2024-10-03 23:03:33 -0500

    Testing cusive table view with dummy values

--- src/tui.rs
+++ src/tui.rs
@@ -1,10 +1,56 @@
-use cursive::{event::Key, menu, views::Dialog};
-use std::fs;
-extern crate cursive_table_view;
+use cursive::{event::Key, menu, view::{Nameable, Resizable}, views::{BoxedView, Dialog, Layer, OnEventView}, View};
+use cursive_table_view::{TableView, TableViewItem};
+use std::{cmp::Ordering, fs};
+use cursive::align::HAlign;
 
-struct SongItem {
-    name: String,
+#[derive(Copy, Clone, PartialEq, Eq, Hash)]
+enum Column {
+    Song,
+    Artist,
+    TrackLength,
 }
+
+#[derive(Clone, Debug)]
+struct SongItem {
+    song: String,
+    artist: String,
+    track_length: usize,
+}
+
+
+impl Column {
+    fn as_str(&self) -> &str {
+        match *self {
+            Column::Song => "Song Name",
+            Column::Artist => "Artist",
+            Column::TrackLength => "Track Length",
+        }
+    }
+}
+
+impl TableViewItem<Column> for SongItem {
+    //converts the column into a string
+    fn to_column(&self, column: Column) -> String {
+        match column {
+            Column::Song => self.song.to_string(),
+            Column::Artist => self.artist.to_string(),
+            Column::TrackLength => format!("{}", self.track_length),
+        }
+    }
+
+    fn cmp(&self, other: &Self, column: Column) -> Ordering
+    where
+        Self: Sized,
+    {
+        match column {
+            Column::Song => self.song.cmp(&other.song),
+            Column::Artist => self.artist.cmp(&other.artist),
+            Column::TrackLength => self.track_length.cmp(&other.track_length),
+        }
+    }
+}
+
+
 
 pub fn list_folders(music_directory: &str) -> Vec<String> {
     fs::read_dir(music_directory) //read the contents of the directory
@@ -55,7 +101,7 @@ pub fn tui_run () {
             albums_menu.add_leaf(album, |_| {});
         }
     }
-        let mut artists_menu = menu::Tree::new(); //empty artist menu
+    let mut artists_menu = menu::Tree::new(); //empty artist menu
 
         for artist in artists { //populate artist menu
             artists_menu.add_leaf(artist, |_| {});
@@ -71,7 +117,31 @@ pub fn tui_run () {
 
     siv.add_global_callback(Key::Esc, |s| s.select_menubar());
 
-    siv.add_layer(Dialog::text("Hit <Esc> to show the menu!"));
+    let mut table = TableView::<SongItem, Column>::new()
+        .column(Column::Song, "Song", |c| c.width_percent(20))
+        .column(Column::Artist, "Artist", |c| c.align(HAlign::Center))
+        .column(Column::TrackLength, "Track Length", |c| {
+            c.ordering(Ordering::Greater)
+                .align(HAlign::Right)
+                .width_percent(20)
+        });
+
+    let mut items = Vec::new();
+        for i in 0..50 {
+            items.push(SongItem {
+                song: format!("Name {}", i),
+                artist: format!("Artist {}", i),
+                track_length: 1,
+        });
+    }
+
+    table.set_items(items);
+
+    siv.add_layer(Dialog::around(table.with_name("table").min_size((50, 20))).title("Table View"));
+
+
+
+    //siv.add_layer(Dialog::text("Hit <Esc> to show the menu!"));
 
     siv.run();
 }
