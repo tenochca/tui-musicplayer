81720ca7d6f70075f6fcb64c0c0b93cab5bac9bb
Diego Izaguirre <tenochca.di@outlook.com>
2024-10-07 17:56:39 -0500

    Function to populate a vector with SongItems structs

--- src/tui.rs
+++ src/tui.rs
@@ -1,6 +1,7 @@
-use cursive::{event::Key, menu, view::{Nameable, Resizable}, views::{BoxedView, Dialog, Layer, OnEventView}, View};
+use audiotags::Tag;
+use cursive::{event::Key, menu, view::{Nameable, Resizable}, views::Dialog};
 use cursive_table_view::{TableView, TableViewItem};
-use std::{cmp::Ordering, fs};
+use std::{cmp::Ordering, fmt::format, fs, path::Path, vec};
 use cursive::align::HAlign;
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash)]
@@ -11,7 +12,7 @@ enum Column {
 }
 
 #[derive(Clone, Debug)]
-struct SongItem {
+pub struct SongItem {
     song: String,
     artist: String,
     track_length: usize,
@@ -66,21 +67,30 @@ pub fn list_folders(music_directory: &str) -> Vec<String> {
     .collect() //collect all valid entries into Vec
 }
 
-pub fn list_files(music_directory: &str) -> Vec<String> {
-    fs::read_dir(music_directory) //read the contents of the directory
-    .unwrap() //handles errors
-    .filter_map(|entry| { //iterates through directory
-        let entry = entry.unwrap();
-        if entry.path().is_file() { //checks if entry is a file
-            Some(entry.file_name().into_string().unwrap()) //valid entries get turned into strings
-        } else {
-            None
-        }
-    })
-    .collect() //collect all valid entries into Vec
+pub fn populate_song_vec(album_directory: &str) -> Vec<SongItem> {
+    let album_path = Path::new(album_directory);
+    let artist = album_path.parent().unwrap().file_name().unwrap().to_str().unwrap().to_string();
+
+    fs::read_dir(album_directory)
+        .unwrap()
+        .filter_map(|entry| {
+            let entry = entry.unwrap();
+            if entry.path().is_file() {
+                let file_path = entry.path();
+                let file_name = file_path.file_name().unwrap().to_str().unwrap().to_string();
+                //let tag = Tag::default().read_from_path(&file_path).unwrap();
+
+                Some(SongItem {
+                    song: file_name,
+                    artist: artist.clone(),
+                    track_length: 1, //placeholder
+                })
+            } else {
+                None
+            }
+        })
+        .collect()
 }
-
-
 
 pub fn tui_run () {
     let mut siv = cursive::default();
@@ -90,26 +100,22 @@ pub fn tui_run () {
 
     //Assume the folders in the dir are all artists & their sub folders are all albums
     let artists = list_folders(music_directory);
-
     let mut albums_menu = menu::Tree::new(); //instantiate empty menu to populate
+    let mut artists_menu = menu::Tree::new(); //empty artist menu
 
     for artist in &artists { //loop through the artist dirs and format new paths to pass to list_folders
         let artist_directory = format!("{}/{}", music_directory, artist);
         let albums = list_folders(&artist_directory);
+        artists_menu.add_leaf(artist, |_| {});
 
         for album in albums { //for each album we populate the menu with it
             albums_menu.add_leaf(album, |_| {});
         }
     }
-    let mut artists_menu = menu::Tree::new(); //empty artist menu
-
-        for artist in artists { //populate artist menu
-            artists_menu.add_leaf(artist, |_| {});
-    }
+    
 
 
-
-    siv.menubar().add_subtree("Albums", albums_menu) 
+    siv.menubar().add_subtree("Albums", albums_menu)
     .add_subtree("Artists", artists_menu)       
     .add_delimiter()
     .add_leaf("Quit", |s| s.quit());
@@ -117,27 +123,22 @@ pub fn tui_run () {
 
     siv.add_global_callback(Key::Esc, |s| s.select_menubar());
 
-    let mut table = TableView::<SongItem, Column>::new()
-        .column(Column::Song, "Song", |c| c.width_percent(20))
+    let mut song_table = TableView::<SongItem, Column>::new()
+        .column(Column::Song, "Track", |c| c.width_percent(20))
         .column(Column::Artist, "Artist", |c| c.align(HAlign::Center))
         .column(Column::TrackLength, "Track Length", |c| {
             c.ordering(Ordering::Greater)
                 .align(HAlign::Right)
                 .width_percent(20)
         });
+        
+        let album_dir: &str = r"C:\Users\tenoc\Music\RadioHead\Fake Plastic Trees";
+        let items = populate_song_vec(album_dir);
+ 
 
-    let mut items = Vec::new();
-        for i in 0..50 {
-            items.push(SongItem {
-                song: format!("Name {}", i),
-                artist: format!("Artist {}", i),
-                track_length: 1,
-        });
-    }
+    song_table.set_items(items);
 
-    table.set_items(items);
-
-    siv.add_layer(Dialog::around(table.with_name("table").min_size((50, 20))).title("Table View"));
+    siv.add_layer(Dialog::around(song_table.with_name("table").min_size((50, 20))).title("Tracks"));
 
 
 
