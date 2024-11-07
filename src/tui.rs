f71b824554fa4c903951cab4f3cfcb74a2557107
Diego Izaguirre <tenochca.di@outlook.com>
2024-11-07 14:26:55 -0600

    Rehaul tui_run function. Cleaned up and seperated into differnt functions

--- src/tui.rs
+++ src/tui.rs
@@ -172,97 +172,8 @@ fn show_popup(s: &mut Cursive, name: &str) {
 pub fn tui_run(music_directory: &str) {
     let mut siv = cursive::default();
 
-    //let music_directory = r"C:/Users/tenoc/Music";
-
-    
-    siv.add_layer(
-        Dialog::new()
-            .title("Enter your music folder")
-            .padding_lrtb(1, 1, 1, 0)
-            .content(
-                EditView::new()
-                    // Call show_popup when the user presses enter
-                    .on_submit(show_popup)
-                    .with_name("name")
-                    .fixed_width(20),
-            )
-            .button("Ok", |s| {
-                let name = s
-                    .call_on_name("name", |view: &mut EditView| {
-                        // return content from closure
-                        view.get_content()
-                    })
-                    .unwrap();
-
-                // Run the next step
-                show_popup(s, &name);
-            }),
-    );
-
-
-    // Assume the folders in the dir are all artists & their sub folders are all albums
-    let artists = list_folders(music_directory);
-    let mut albums_menu = menu::Tree::new(); // Instantiate empty menu to populate
-    let mut artists_menu = menu::Tree::new(); // Empty artist menu
-
-    for artist in &artists { // Loop through the artist dirs and format new paths to pass to list_folders
-        let artist_directory = format!("{}/{}", music_directory, artist);
-        let albums = list_folders(&artist_directory);
-        let artist_name = artist.clone();
-        let artist_directory_clone = artist_directory.clone();
-        artists_menu.add_leaf(artist, move |s| {
-            s.pop_layer();
-            let album_dialog = Dialog::around(TableView::<AlbumItem, Column>::new()
-                .column(Column::Album, "Album", |c| c.width_percent(50))
-                .column(Column::Artist, "Artist", |c| c.align(HAlign::Center).width_percent(50))
-                .with_name("album_table")
-                .min_size((50, 20)))
-                .title(&format!("Albums - {}", artist_name))
-                .with_name("album_dialog");
-            s.add_layer(album_dialog);
-            let items = populate_album_vec(&artist_directory_clone);
-            //println!("{:?}", items);
-            s.call_on_name("album_table", |table: &mut TableView<AlbumItem, Column>| {
-                table.set_items(items);
-            });
-        });
-
-        for album in &albums {
-            let album_directory = format!("{}/{}", artist_directory, album);
-            let album_name = album.clone();
-            let album_directory_clone = Arc::new(album_directory.clone()); // Wrap in Arc
-            albums_menu.add_leaf(album, move |s| {
-                s.pop_layer();
-                let song_dialog = Dialog::around(TableView::<SongItem, Column>::new()
-                    .column(Column::Song, "Track", |c| c.width_percent(35))
-                    .column(Column::Artist, "Artist", |c| c.align(HAlign::Center).width_percent(20))
-                    .column(Column::TrackLength, "Track Length", |c| {
-                        c.ordering(Ordering::Greater)
-                            .align(HAlign::Right)
-                            .width_percent(40)
-                    })
-                    .on_submit({
-                        let album_directory_clone = Arc::clone(&album_directory_clone); // Clone the Arc
-                        move |s, row, _index| {
-                            let song_table = s.find_name::<TableView<SongItem, Column>>("song_table").unwrap();
-                            let song_item = song_table.borrow_item(row).unwrap();
-                            let song_path = format!("{}/{}", album_directory_clone, song_item.song); // Use the cloned Arc
-                            //println!("{}", song_path);
-                            audio::play_audio(&song_path, s,  song_item);
-                        }
-                    })
-                    .with_name("song_table")
-                    .min_size((50, 20)))
-                    .title(&format!("Tracks - {}", album_name))
-                    .with_name("song_dialog");
-                s.add_layer(song_dialog);
-                let items = populate_song_vec(&album_directory);
-                s.call_on_name("song_table", |table: &mut TableView<SongItem, Column>| {
-                    table.set_items(items);
-                });
-            });
-        }
-    }
+    add_initial_dialog(&mut siv);
+    let (artists_menu, albums_menu) = build_menus(music_directory);
 
     siv.menubar().add_subtree("Artists", artists_menu)
         .add_subtree("Albums", albums_menu)
@@ -271,8 +182,98 @@ pub fn tui_run(music_directory: &str) {
 
     siv.add_global_callback(Key::Esc, |s| s.select_menubar());
 
-    //siv.add_layer(Dialog::around(album_table.min_size((50, 20))).title("Albums").with_name("dialog"));
-    //siv.add_layer(Dialog::around(song_table.min_size((50, 20))).title("Tracks").with_name("dialog"));
-
     siv.run();
+}
+
+fn add_initial_dialog(siv: &mut Cursive) {
+    siv.add_layer(
+        Dialog::new()
+            .title("Enter your music folder")
+            .padding_lrtb(1, 1, 1, 0)
+            .content(
+                EditView::new()
+                    .on_submit(show_popup)
+                    .with_name("name")
+                    .fixed_width(20),
+            )
+            .button("Ok", |s| {
+                let name = s
+                    .call_on_name("name", |view: &mut EditView| {
+                        view.get_content()
+                    })
+                    .unwrap();
+                show_popup(s, &name);
+            }),
+    );
+}
+
+fn build_menus(music_directory: &str) -> (menu::Tree, menu::Tree) {
+    let artists = list_folders(music_directory);
+    let mut albums_menu = menu::Tree::new();
+    let mut artists_menu = menu::Tree::new();
+
+    for artist in &artists {
+        let artist_directory = format!("{}/{}", music_directory, artist);
+        let albums = list_folders(&artist_directory);
+        let artist_name = artist.clone();
+        let artist_directory_clone = artist_directory.clone();
+        artists_menu.add_leaf(artist, move |s| {
+            show_albums(s, &artist_name, &artist_directory_clone);
+        });
+
+        for album in &albums {
+            let album_directory = format!("{}/{}", artist_directory, album);
+            let album_name = album.clone();
+            let album_directory_clone = Arc::new(album_directory.clone());
+            albums_menu.add_leaf(album, move |s| {
+                show_songs(s, &album_name, Arc::clone(&album_directory_clone));
+            });
+        }
+    }
+
+    (artists_menu, albums_menu)
+}
+
+fn show_albums(s: &mut Cursive, artist_name: &str, artist_directory: &str) {
+    let album_dialog = Dialog::around(TableView::<AlbumItem, Column>::new()
+        .column(Column::Album, "Album", |c| c.width_percent(50))
+        .column(Column::Artist, "Artist", |c| c.align(HAlign::Center).width_percent(50))
+        .with_name("album_table")
+        .min_size((50, 20)))
+        .title(&format!("Albums - {}", artist_name))
+        .with_name("album_dialog");
+    s.add_layer(album_dialog);
+    let items = populate_album_vec(artist_directory);
+    s.call_on_name("album_table", |table: &mut TableView<AlbumItem, Column>| {
+        table.set_items(items);
+    });
+}
+
+fn show_songs(s: &mut Cursive, album_name: &str, album_directory: Arc<String>) {
+    let song_dialog = Dialog::around(TableView::<SongItem, Column>::new()
+        .column(Column::Song, "Track", |c| c.width_percent(35))
+        .column(Column::Artist, "Artist", |c| c.align(HAlign::Center).width_percent(20))
+        .column(Column::TrackLength, "Track Length", |c| {
+            c.ordering(Ordering::Greater)
+                .align(HAlign::Right)
+                .width_percent(40)
+        })
+        .on_submit({
+            let album_directory_clone = Arc::clone(&album_directory);
+            move |s, row, _index| {
+                let song_table = s.find_name::<TableView<SongItem, Column>>("song_table").unwrap();
+                let song_item = song_table.borrow_item(row).unwrap();
+                let song_path = format!("{}/{}", album_directory_clone, song_item.song);
+                audio::play_audio(&song_path, s, song_item);
+            }
+        })
+        .with_name("song_table")
+        .min_size((50, 20)))
+        .title(&format!("Tracks - {}", album_name))
+        .with_name("song_dialog");
+    s.add_layer(song_dialog);
+    let items = populate_song_vec(&album_directory);
+    s.call_on_name("song_table", |table: &mut TableView<SongItem, Column>| {
+        table.set_items(items);
+    });
 }
